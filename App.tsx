78e15b723703bac14df1deaaf4638bd7022239c6
Daniel Friyia <dfriyia@gmail.com>
2024-07-01 23:38:00 -0400

    Collisions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { Canvas, Circle, Rect } from "@shopify/react-native-skia";
-import React from "react";
-import { Dimensions, SafeAreaView, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from "react-native-gesture-handler";
 import { useFrameCallback, useSharedValue } from "react-native-reanimated";
+import {
+  ShapeInterface,
+  animate,
+  createBouncingExample,
+  radius,
+} from "./sample";
 
 const LIME_GREEN = "#4AF626";
 
@@ -15,11 +21,8 @@ const BOTTOM = height - 120;
 const PADDLE_HEIGHT = 20;
 const PADDLE_Y = BOTTOM - PADDLE_HEIGHT;
 const TOP = 10;
-
 // 10 frames every 16 milliseconds
 const GAME_SPEED = 10 / 16;
-
-const FRAME_UPDATE_TIME = 16.6;
 
 interface Point {
   x: number;
@@ -33,71 +36,134 @@ const dist = (point1: Point, point2: Point) => {
   );
 };
 
-export default function App() {
-  const isDecending = useSharedValue(true);
-  const paddleX = useSharedValue(100);
-  const circleX = useSharedValue(100);
-  const circleY = useSharedValue(0);
+interface CircleProps {
+  circlePosition: ShapeInterface;
+}
 
-  const gesture = Gesture.Pan().onChange(({ x }) => {
-    paddleX.value = x;
-  });
+const MovingCircle = ({ circlePosition }: CircleProps) => {
+  return (
+    <Circle
+      cx={circlePosition.x}
+      cy={circlePosition.y}
+      r={radius}
+      color={LIME_GREEN}
+    />
+  );
+};
+
+export default function App() {
+  const isLoaded = useSharedValue(false);
+
+  const circleObjects: ShapeInterface[] = Array(16)
+    .fill(0)
+    .map((_, i) => {
+      return {
+        id: i,
+        x: useSharedValue(0),
+        y: useSharedValue(0),
+        r: radius,
+        m: 0,
+        ax: 0,
+        ay: 0,
+        vx: 0,
+        vy: 0,
+      };
+    });
+
+  useEffect(() => {
+    createBouncingExample(circleObjects);
+    isLoaded.value = true;
+  }, []);
 
   useFrameCallback((frameInfo) => {
-    if (!frameInfo.timeSincePreviousFrame) {
+    if (!isLoaded.value) {
       return;
     }
-
-    const distance = dist(
-      { x: circleX.value, y: circleY.value },
-      {
-        x: paddleX.value,
-        y: PADDLE_Y,
-      }
-    );
-
-    if (distance <= PADDLE_HEIGHT) {
-      isDecending.value = false;
-      circleY.value -= frameInfo.timeSincePreviousFrame * GAME_SPEED;
-      return;
-    }
-
-    // This can probably be removed later.
-    // You lose the game if this happens
-    if (circleY.value >= BOTTOM) {
-      isDecending.value = false;
-    }
-
-    if (circleY.value <= TOP) {
-      isDecending.value = true;
-    }
-
-    if (isDecending.value) {
-      circleY.value += frameInfo.timeSincePreviousFrame * GAME_SPEED;
-    } else {
-      circleY.value -= frameInfo.timeSincePreviousFrame * GAME_SPEED;
-    }
+    animate(circleObjects);
   });
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <GestureDetector gesture={gesture}>
-        <SafeAreaView style={styles.container}>
-          <Canvas style={{ height: BOTTOM }}>
-            <Circle cx={100} cy={circleY} color={LIME_GREEN} r={TOP} />
-            <Rect
-              x={paddleX}
-              y={PADDLE_Y}
-              height={PADDLE_HEIGHT}
-              width={100}
-              color={"white"}
-            />
-          </Canvas>
-        </SafeAreaView>
-      </GestureDetector>
+      <View style={styles.container}>
+        <Canvas style={{ flex: 1 }}>
+          {Array(16)
+            .fill(0)
+            .map((_, idx) => {
+              return (
+                <MovingCircle key={idx} circlePosition={circleObjects[idx]} />
+              );
+            })}
+        </Canvas>
+      </View>
     </GestureHandlerRootView>
   );
 }
+
+// export default function App() {
+//   const isDecending = useSharedValue(true);
+//   const paddleX = useSharedValue(100);
+//   const circleX = useSharedValue(100);
+//   const circleY = useSharedValue(0);
+
+//   const gesture = Gesture.Pan().onChange(({ x }) => {
+//     paddleX.value = x;
+//   });
+
+//   useFrameCallback((frameInfo) => {
+//     if (!frameInfo.timeSincePreviousFrame) {
+//       return;
+//     }
+
+//     const distance = dist(
+//       { x: circleX.value, y: circleY.value },
+//       {
+//         x: paddleX.value,
+//         y: PADDLE_Y,
+//       }
+//     );
+
+//     if (distance <= PADDLE_HEIGHT) {
+//       isDecending.value = false;
+//       circleY.value -= frameInfo.timeSincePreviousFrame * GAME_SPEED;
+//       return;
+//     }
+
+//     // This can probably be removed later.
+//     // You lose the game if this happens
+//     if (circleY.value >= BOTTOM) {
+//       isDecending.value = false;
+//     }
+
+//     if (circleY.value <= TOP) {
+//       isDecending.value = true;
+//     }
+
+//     if (isDecending.value) {
+//       circleY.value += frameInfo.timeSincePreviousFrame * GAME_SPEED;
+//     } else {
+//       circleY.value -= frameInfo.timeSincePreviousFrame * GAME_SPEED;
+//     }
+//   });
+
+//   return (
+//     <GestureHandlerRootView style={{ flex: 1 }}>
+//       <GestureDetector gesture={gesture}>
+//         <SafeAreaView style={styles.container}>
+//           <Canvas style={{ height: BOTTOM }}>
+//             <Circle cx={100} cy={circleY} color={LIME_GREEN} r={TOP} />
+//             <Rect
+//               x={paddleX}
+//               y={PADDLE_Y}
+//               height={PADDLE_HEIGHT}
+//               width={100}
+//               color={"white"}
+//             />
+//           </Canvas>
+//         </SafeAreaView>
+//       </GestureDetector>
+//     </GestureHandlerRootView>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
---
+++ sample.ts
@@ -0,0 +1,226 @@
+import { Dimensions } from "react-native";
+import { SharedValue } from "react-native-reanimated";
+
+const { width, height } = Dimensions.get("window");
+
+function getRandomInt(min: number, max: number) {
+  "worklet";
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const radius = 16;
+const maxSpeed = 150;
+
+export interface ShapeInterface {
+  id: number;
+  /**
+   * `x` position of the shape on the canvas
+   */
+  x: SharedValue<number>;
+  /**
+   * `y` position of the shape on the canvas
+   */
+  y: SharedValue<number>;
+  /**
+   * The radius of the shape
+   */
+  r: number;
+  /**
+   * The mass of the shape
+   */
+  m: number;
+  /**
+   * The acceleration of x
+   */
+  ax: number;
+  /**
+   * The acceleration of y
+   */
+  ay: number;
+  /**
+   * The velocity of x
+   */
+  vx: number;
+  /**
+   * The velocity of y
+   */
+  vy: number;
+}
+
+/**
+ * Object representing the collision between two objects
+ */
+interface Collision {
+  /**
+   * The first shape
+   */
+  o1: ShapeInterface;
+  /**
+   * The second shape
+   */
+  o2: ShapeInterface;
+  /**
+   * Distance between the two x values
+   */
+  dx: number;
+  /**
+   * Distance betweeb the two y values
+   */
+  dy: number;
+  /**
+   * Total distance
+   */
+  d: number;
+}
+
+const move = (object: ShapeInterface, dt: number) => {
+  "worklet";
+  object.vx += object.ax * dt;
+  object.vy += object.ay * dt;
+  if (object.vx > maxSpeed) {
+    object.vx = maxSpeed;
+  }
+  if (object.vx < -maxSpeed) {
+    object.vx = -maxSpeed;
+  }
+  if (object.vy > maxSpeed) {
+    object.vy = maxSpeed;
+  }
+  if (object.vy < -maxSpeed) {
+    object.vy = -maxSpeed;
+  }
+  object.x.value += object.vx * dt;
+  object.y.value += object.vy * dt;
+};
+
+export const resolveWallCollision = (object: ShapeInterface) => {
+  "worklet";
+  // Collision with the right wall
+  if (object.x.value + object.r > width) {
+    // Calculate the overshot
+    object.x.value = width - object.r;
+    object.vx = -object.vx;
+    object.ax = -object.ax;
+  }
+
+  // Collision with the bottom wall
+  else if (object.y.value + object.r > height) {
+    object.y.value = height - object.r;
+    object.vy = -object.vy;
+    object.ay = -object.ay;
+  }
+
+  // Collision with the left wall
+  else if (object.x.value - object.r < 0) {
+    object.x.value = object.r;
+    object.vx = -object.vx;
+    object.ax = -object.ax;
+  }
+
+  // Detect collision with the top wall
+  else if (object.y.value - object.r < 0) {
+    object.y.value = object.r;
+    object.vy = -object.vy;
+    object.ay = -object.ay;
+  }
+};
+
+export const createBouncingExample = (circleObjects: ShapeInterface[]) => {
+  "worklet";
+  for (let i = 0; i < 16; i++) {
+    const x = getRandomInt(radius, width - radius);
+    const y = getRandomInt(radius, height - radius);
+
+    circleObjects[i].x.value = x;
+    circleObjects[i].y.value = y;
+    circleObjects[i].r = radius;
+    circleObjects[i].ax = getRandomInt(-1, 1);
+    circleObjects[i].ay = getRandomInt(-1, 1);
+    circleObjects[i].m = radius * 10;
+  }
+};
+
+export const checkCollision = (o1: ShapeInterface, o2: ShapeInterface) => {
+  "worklet";
+  const dx = o2.x.value - o1.x.value;
+  const dy = o2.y.value - o1.y.value;
+  const d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+  if (d < o1.r + o2.r) {
+    return {
+      collisionInfo: { o1, o2, dx, dy, d },
+      collided: true,
+    };
+  } else {
+    return {
+      collisionInfo: null,
+      collided: false,
+    };
+  }
+};
+
+export const resolveCollisionWithBounce = (info: Collision) => {
+  "worklet";
+  // Eigen Vector
+  const nx = info.dx / info.d;
+  const ny = info.dy / info.d;
+
+  // Penetration Depth
+  const s = info.o1.r + info.o2.r - info.d;
+
+  info.o1.x.value -= (nx * s) / 2;
+  info.o1.y.value -= (ny * s) / 2;
+
+  info.o2.x.value += (nx * s) / 2;
+  info.o2.y.value += (ny * s) / 2;
+
+  const k =
+    (-2 * ((info.o2.vx - info.o1.vx) * nx + (info.o2.vy - info.o1.vy) * ny)) /
+    (1 / info.o1.m + 1 / info.o2.m);
+
+  info.o1.vx -= (k * nx) / info.o1.m;
+  info.o1.vy -= (k * ny) / info.o1.m;
+
+  info.o2.vx += (k * nx) / info.o2.m;
+  info.o2.vy += (k * ny) / info.o2.m;
+};
+
+export const animate = (objects: ShapeInterface[]) => {
+  "worklet";
+  for (const o of objects) {
+    move(o, 0.1);
+  }
+
+  for (const o of objects) {
+    resolveWallCollision(o);
+  }
+
+  const collisions: Collision[] = [];
+
+  for (const [i, o1] of objects.entries()) {
+    for (const [j, o2] of objects.entries()) {
+      if (i < j) {
+        const { collided, collisionInfo } = checkCollision(o1, o2);
+        if (collided && collisionInfo) {
+          collisions.push(collisionInfo);
+        }
+      }
+    }
+  }
+
+  for (const col of collisions) {
+    resolveCollisionWithBounce(col);
+  }
+
+  // for (const o of objects) {
+  //   o.draw();
+  // }
+
+  // window.requestAnimationFrame(animate);
+};
+
+// document.getElementById("bounceExample")!.onclick = createBouncingExample;
+
+// window.requestAnimationFrame(animate);
